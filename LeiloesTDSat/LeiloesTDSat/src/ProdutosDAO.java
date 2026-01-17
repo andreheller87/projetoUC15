25bb2569124aa973735c341fd18443ad6685ec2c
Andre Heller <andreheller02@gmail.com>
2026-01-16 22:15:10 -0300

    listagem de produtos funcionando

--- LeiloesTDSat/LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/LeiloesTDSat/src/ProdutosDAO.java
@@ -44,8 +44,31 @@ public class ProdutosDAO {
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
+         String sql = "SELECT * FROM produtos";
+
+    conn = new conectaDAO().connectDB();
+    listagem = new ArrayList<>();
+
+    try {
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+
+        while (resultset.next()) {
+            ProdutosDTO produto = new ProdutosDTO();
+
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+
+            listagem.add(produto);
+        }
+
+    } catch (Exception erro) {
+        JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + erro);
+    }
+
+    return listagem;
     }
     
     
